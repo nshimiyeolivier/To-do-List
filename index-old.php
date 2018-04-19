5959af203bdbef1cf7021d10ac07a23b309bd72d
NSHIMIYUMUKIZA Olivier <nchima2010@gmail.com>
2018-04-19 10:18:53 +0200

    Update index-old.php

--- index-old.php
+++ index-old.php
@@ -55,7 +55,7 @@ if( (isset($_POST["submit-modif"])) ) {
 }
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
